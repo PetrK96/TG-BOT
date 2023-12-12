46d4277e8247c235c60ee5266ff6f21e3567ed2a
Petr Kulikov <peterkulikoff@yandex.ru>
2023-12-12 15:59:06 +0300

    first bot, basic count

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("This is new file")
-print("This is new file")
+import requests
+import time
+
+API_URL = "https://api.telegram.org/bot"
+BOT_TOKEN = "6704589031:AAFuUU94ZpKZPO6CzIHvQzCxXJJDuYBCNqI"
+TEXT = "Ура, замечено обновление"
+MAX_COUNTER = 100
+
+offset = -2 #Что это??
+counter = 0
+chat_id: int
+
+while counter < MAX_COUNTER:
+    print('attempt = ', counter) #чтобы видеть в консоли кол-во повторений в консоле
+    updates = requests.get('{}{}/getUpdates?offset={}'.format(API_URL, BOT_TOKEN, offset + 1)).json()
+
+    if updates["result"]:
+        for result in updates['result']:
+            offset = result["update_id"]
+            chat_id = result['message']["from"]["id"]
+            name = result['message']['from']['first_name'] + " " + result['message']['from']['last_name']
+
+            requests.get("{}{}/sendMessage?chat_id={}&text={}, {}".format(API_URL, BOT_TOKEN, chat_id, TEXT, name))
+
+    time.sleep(1)
+    counter += 1
+
